ce4859884a0285b3b76228b6ce5cb81e8e696fff
SugarSyrup <46832146+SugarSyrup@users.noreply.github.com>
2021-10-09 08:50:19 +0900

    2021.10.09 Init
    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import React from "react";
+
+function Food(props){
+  return <h1>TEST {props.favorite}!!</h1>;
+}
+
+function renderFood(props){
+  return (
+    <Food 
+  )
+}
+
+const foodList = ["Sugar","Candy","CottonCandy"];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <Food favorite="potato" />
+      {foodList.map(function (favs){
+        return (<Food favorite={favs} />);
+      })}
     </div>
   );
 }
