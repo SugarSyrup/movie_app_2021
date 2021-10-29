045498084ef68c7f2512581408ad54bd80839143
SugarSyrup <tlfvm04@naver.com>
2021-10-29 15:42:19 +0900

    #2..
    2021.10.29

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
 //import './App.css';
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
-function Food(props){
-  return <h1>TEST {props.favorite}!!</h1>;
-}
+import "./App.css";
 
-function renderFood(props){
-  return (
-    <Food 
-  )
-}
+class App extends React.Component{
+  state = {
+    isLoading : true,
+    movies: []
+  };
 
-const foodList = ["Sugar","Candy","CottonCandy"];
+  getMovies = async () => {
+    //const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    const {data: {data: {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    //this.setState({movies:movies});
+    this.setState({ movies, isLoading: false });
+  }
+  componentDidMount(){
+    this.getMovies();
+  }
 
-function App() {
-  return (
-    <div className="App"> 
-      <Food favorite="potato" />
-      {foodList.map(function (favs){
-        return (<Food favorite={favs} />);
-      })}
-    </div>
-  );
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+          ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} genres={movie.genres}/>
+            ))}
+          </div>
+        )}
+      </section>
+    )}
+
+  componentDidUpdate(){
+  }
+  componentWillUnmount(){
+  }
 }
 
 export default App;
